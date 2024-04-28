564fff6fdaca4c91edfb2eb5eaeeb944e5d02842
enesburak <enesburakcetin1907@gmail.com>
2024-04-28 17:34:34 +0300

    12 Buton

---
+++ app/src/main/java/com/example/projectapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.projectapp;
+
+import android.content.Intent; //  Intent Library
+import android.os.Bundle;
+import android.widget.Button;  // Button library
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import android.view.View;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button  button1,button2,button3,button4,button5,button6,button7,button8,button9,button10,button11,button12;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        button1=findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new  Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+
+        button2 =findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity3.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
